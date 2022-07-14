f6a3f29adf2bbf1cce7f429312995877dddb1023
Nikita Vijay <nikita.vijay@metacube.com>
2022-07-15 03:59:16 +0530

    Code Committedgit status  New function for square pattern addedgit status

--- src/classes/patterns.php
+++ src/classes/patterns.php
@@ -1,22 +1,38 @@
 <?php
 class Pattern{
-
+    public $n = 25;
     public function __construct() {}
 
     public function getPyramidPattern() {
-        $n = 5;
-        for($i = 0; $i < $n; $i++){
-            for($j = 1; $j <= $n; $j++){
-                if($j+$i < $n){
+        for($i = 0; $i < $this->n; $i++){
+            for($j = 1; $j <= $this->n; $j++){
+                if($j+$i < $this->n){
                     echo "&nbsp;";
                 }else{
                     echo "*";
                 }
             }
-            for($k = 1; $k <= $n; $k++){
+            for($k = 1; $k <= $this->n; $k++){
                 if($k == $i){
                     echo "*";
                 }
+            }
+            echo "<br>";
+        }
+
+        return;
+    }
+
+    public function getSquarePattern() {
+        
+        for($i = 1; $i <= $this->n; $i++){
+            for($j = 1; $j <= $this->n; $j++){
+                if($i == 1 || $i == $this->n || $j == 1 || $j == $this->n){
+                    echo "*";
+                }else{
+                    echo '&nbsp;&nbsp;';
+                }
+                
             }
             echo "<br>";
         }
